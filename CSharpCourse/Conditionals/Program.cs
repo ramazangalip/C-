716490a43e2d02b9381b58315889adabe1bcd820
Ramazan  Said GALİP <107424417+ramazangalip@users.noreply.github.com>
2024-06-25 22:53:41 +0300

    25.06.2024

---
+++ CSharpCourse/Conditionals/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 20;
+            //if (number==10)
+            //{
+            //    Console.WriteLine("Sayi 10 dur");
+            //}
+            //else if(number == 20)
+            //{
+            //    Console.WriteLine("Sayi 20 dir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sayi 10 değildir");
+            //}
+
+            //switch (number)
+            //{
+            //    case 10:
+            //        Console.WriteLine("Sayi 10 dur");
+            //        break;
+            //    case 20:
+            //        Console.WriteLine("Sayi 20 dir");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Sayi 10 ya da 20 değil");
+            //        break;
+            //}
+
+            //if (number >= 0 && number <= 100)
+            //{
+            //    Console.WriteLine("Sayi 0-100 arasındadır");
+            //}
+            //else if (number > 100 && number <= 200)
+            //{
+            //    Console.WriteLine("Sayi 101-200 arasındadır");
+            //}
+            //else if (number > 200 || number < 0)
+            //{
+            //    Console.WriteLine("Sayi 200 den büyük veya 0 dan küçük");
+            //}
+            if (number<100)
+            {
+                if (number>=90&& number<95)
+                {
+                    
+                }
+                else
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpCourse/TypesAndVariables/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Value Types
+            
+            double number5 = 10.4;
+            decimal number6 = 10.4M;
+            char character = 'A';
+            bool condition1 = true;
+            bool condition2 = false;
+            byte number4 = 255;
+            short number3 = 10000;
+            int number1 = 1000000000;
+            long number2 = 1000000000000000000;
+            var number7 = 10;
+          
+            Console.WriteLine("1.sayi " + number1);
+            Console.WriteLine("2.sayi " + number2);
+            Console.WriteLine("3.sayi " + number3);
+            Console.WriteLine("4.sayi " + number4);
+            Console.WriteLine("5.sayi " + number5);
+            Console.WriteLine("Character is {0}",(int)character);
+            Console.WriteLine((int)Days.Cuma);
+            Console.ReadLine();
+        }
+    }
+    enum Days
+    {
+        Pazartesi,Salı,Çarşamba,Perşembe, Cuma,Cumartesi,Pazar
+    }
+}
